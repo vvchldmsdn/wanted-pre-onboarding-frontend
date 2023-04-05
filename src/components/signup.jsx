95a55f19fc8def3bd3398c53a5b78a19805661bf
vvchldmsdn <vvchldmsdn@gmail.com>
2023-04-06 00:20:21 +0900

    signup assignment2

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './signup.css'
 import Toast from 'react-bootstrap/Toast';
+import axios from "axios";
 
 function SignUp() {
   const [email, setEmail] = useState('');
@@ -16,6 +17,24 @@ function SignUp() {
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword);
+  }
+
+  const handleSingupButtonClick = () => {
+    console.log('email', email)
+    console.log('password', password)
+    const data = {
+      email: email,
+      password: password
+    };
+    const url = 'https://www.pre-onboarding-selection-task.shop/auth/signup'
+
+    axios.post(url, data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
   }
 
   useEffect(() => {
@@ -51,12 +70,12 @@ function SignUp() {
       <div className='button-signup'>
         <button type="button" class="btn btn-outline-dark"
           disabled={disabled}
+          onClick={handleSingupButtonClick}
         >회원가입</button>
       </div>
-      <Toast className='toast-signup'
-        show={showToast}
+      <Toast className='toast-signup' show={showToast}
       >
-        <Toast.Body>이메일은 @를 반드시 포함해야합니다. / 비밀번호는 8자리 이상이여야합니다.</Toast.Body>
+        <Toast.Body>이메일은 @를 반드시 포함해야합니다 / 비밀번호는 8자리 이상이여야합니다</Toast.Body>
       </Toast>
     </div>
   );
